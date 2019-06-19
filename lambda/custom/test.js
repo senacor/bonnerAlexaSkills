0ea22d87f049925e1baf647e0bd9dc9fe55c46fe
agoliath <agoliath>
2019-06-19 14:56:22 +0200

    added test code

---
+++ lambda/custom/test.js
@@ -0,0 +1,47 @@
+let handler = require("./index").debug;
+
+const getMockResponseBuilder = () => {
+    const that = {
+        speak: (speechText) => {
+            that.speechText = speechText;
+            return that;
+        },
+        reprompt: (repromptText) => {
+            that.repromptText = repromptText;
+            return that;
+        },
+        withSimpleCard: (title, cardText) => {
+            that.simpleCardTitle = title;
+            that.simpleCardText = cardText;
+            return that;
+        },
+        withShouldEndSession: (shouldEndSession) => {
+            that.shouldEndSession = shouldEndSession;
+            return that;
+        },
+        withAskForPermissionsConsentCard: (permissions) => {
+            that.requestedPermissions = permissions;
+            return that;
+        },
+        addDelegateDirective: (updatedIntent) => {
+            const mockResponse = {
+                ...that,
+                updatedIntent
+            };
+            return mockResponse;
+        },
+        getResponse: () => that,
+        speechText: '',
+        repromptText: '',
+        simpleCardText: '',
+        simpleCardTitle: '',
+        shouldEndSession: undefined,
+        updatedIntent: undefined,
+        hasDelegateDirective: false
+    };
+    return that;
+};
+
+let out = handler.HelloWorldIntentHandler.handle({responseBuilder: getMockResponseBuilder()});
+
+console.log(out);
